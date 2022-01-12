0e46f70fa3cb58b1471d5bcc151044580fc3f931
Srajan Poojary <74640796+stressunfit-design@users.noreply.github.com>
2022-01-12 22:36:09 +0530

    Create MaximumConsecutive1.java

---
+++ Array/BasicArrayQuestions/MaximumConsecutive1.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+class GFG 
+{ 
+    
+
+    static int maxConsecutiveOnes(int arr[], int n)
+    {
+    	int res = 0;
+
+    	for(int i = 0; i < n; i++)
+    	{
+    		int curr = 0;
+
+    		for(int j = i; j < n; j++)
+    		{
+    			if(arr[j] == 1) curr++;
+    			else break;
+    		}
+
+    		res = Math.max(res, curr);
+    	}
+    	
+    	return res;
+    }
+
+
+    public static void main(String args[]) 
+    { 
+       int arr[] = {0, 1, 1, 1, 0, 1, 1}, n = 7;
+
+       System.out.println(maxConsecutiveOnes(arr, n));
+
+    } 
